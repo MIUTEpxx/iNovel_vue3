c09c2c818824e5c6ee7204026e43ff5796a6cba3
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-09-25 19:05:02 +0800

    安装依赖,搭建环境

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    open:true,//运行 npm run dev后自动打开
+    port:8088
+  },
   plugins: [vue()],
 })
