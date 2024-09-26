84980219dbd4402657a8ef9fdf40d7116a48c70b
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-09-26 15:49:04 +0800

    底部导航栏完善

---
+++ src/views/navbottom.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+
+        <div class="footer">
+            <div class="footer-seat">
+                <div class="footer-list">
+                    <ul>
+                        <li>书城</li>
+                        <li>社区</li>
+                        <li>福利</li>
+                        <li>我的</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+</template>
+
+<!-- scoped表示该样式是局域样式,不会影响其他组件 -->
+<style scoped>
+
+
+</style>
