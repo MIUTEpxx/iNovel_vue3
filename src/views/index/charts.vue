b940ff44a2ce34079ff0bb091fa87bad7d4c2772
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-09-27 11:19:50 +0800

    排行榜

---
+++ src/views/index/charts.vue
@@ -0,0 +1,16 @@
+<!-- 排行榜 -->
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+        <div class="charts">
+           <h4>排行榜</h4>
+           <div>热门分类</div>
+        </div>
+</template>
+
+<style scoped>
+
+
+</style>
