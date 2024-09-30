49a32d6d5d689136156cabbcec8b797712f53bee
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-09-30 15:29:13 +0800

    完善轮播图

--- src/views/index/banner.vue
+++ src/views/index/banner.vue
@@ -3,25 +3,20 @@
   <!-- 轮播图 -->
 <script setup>
 import { ref } from 'vue';
+let bannerList=ref([
+    {id:1,img_url:'banner1.jpg'},
+    {id:2,img_url:'banner2.jpg'},
+    {id:3,img_url:'banner3.jpg'},
+    {id:4,img_url:'banner4.jpg'},
+    {id:5,img_url:'banner5.jpg'},
+])
 </script>
 
 <template>
         <div class="banner">
             <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-                <van-swipe-item>
-                    <img src="@/assets/images/banner1.jpg" alt="">
-                </van-swipe-item>
-                <van-swipe-item>
-                    <img src="@/assets/images/banner2.jpg" alt="">
-                </van-swipe-item>
-                <van-swipe-item>
-                    <img src="@/assets/images/banner3.jpg" alt="">
-                </van-swipe-item>
-                <van-swipe-item>
-                    <img src="@/assets/images/banner4.jpg" alt="">
-                </van-swipe-item>
-                <van-swipe-item>
-                    <img src="@/assets/images/banner5.jpg" alt="">
+                <van-swipe-item v-for="v in bannerList" :key="v.id">
+                    <img :src="`/src/assets/images/${v.img_url}`" alt="">
                 </van-swipe-item>
             </van-swipe>
         </div>
