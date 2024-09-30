ab3be9f25b272f16e26953ccd65bed9c54829b36
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-09-30 22:16:59 +0800

    完善喜好设置

--- src/views/index/like.vue
+++ src/views/index/like.vue
@@ -18,7 +18,30 @@ const show = ref(false);
         close-icon-position="top-left"
         position="bottom"
         :style="{ height: '60%' }">
-        1111
+        <div class="like-list">
+            <h4>请选择您感兴趣的标签</h4>
+            <p>请选择喜欢的分类:(已选择 个)</p>
+            <ul>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+                <li>奇幻</li>
+            </ul>
+        </div>
     </van-popup>
 
     </div>
