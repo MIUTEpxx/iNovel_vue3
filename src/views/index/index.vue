d9b1bfe5a490c0304b937c20db673e2ebbaec609
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-10-01 16:05:15 +0800

    开屏广告完善

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,17 +1,34 @@
 <!-- 书城首页 -->
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import Header from './header.vue';
 import Charts from './charts.vue';
 import Chartstab from './chartstab.vue';
 import Banner from './banner.vue';
 import Recommend from './recommend.vue';
+
+let adBack=ref('true')//开屏广告的显示和隐藏
+let n =ref(5)//默认时间 秒
+let timer=null;
+function play(){
+        n.value--;
+        console.log(n.value);
+        if(n.value<=0){
+                adBack.value=false;
+        }
+};
+function autoPlay(){
+        timer=setInterval(play,1000);//定时器 1s
+}
+onMounted(()=>{
+        autoPlay();
+})
 </script>
 
 <template>
         <!-- 开屏广告 -->
-         <div class="spread">
-                <span class="jump">点击跳转 <b>5</b> 秒</span>
+         <div class="spread" v-show="adBack" @click="adBack=false">
+                <span class="jump">点击跳过 <b>{{n}}</b> 秒</span>
          </div>
          <!-- 首页 -->
         <div class="container">
