ba30f37ff575622c35c6cfdc5aa768aa75e86236
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-10-02 15:27:11 +0800

    完善个人页面头部

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -3,23 +3,41 @@ import { ref } from 'vue';
 </script>
 
 <template>
-
-        <div class="my">
-         <div class="my-hd">
-              <div class="my-hd-login">
-                  <div class="my-hd-img">
-                    <img src="" alt="头像">
-                  </div>
-                  <div class="my-hd-item">
-                    <div>游客</div>
-                    <button>点击登录</button>
-                  </div>
+  <div class="my">
+      <div class="my-hd">
+          <div class="my-hd-login">
+              <div class="my-hd-img">
+                  <img src="@/assets/images/h.jpg" alt="头像">
               </div>
-         </div>
-         <div class="my-body">
+              <div class="my-hd-item">
+                  <div class="username">游客</div>
+                  <button>点击登录</button>
+              </div>
+          </div>
+      </div>
+      <div class="my-body">
+          <div class="my-body-item1">
+              <ul>
+                <li>
+                  <i class="iconfont icon-jinbi"></i>
+                  <p>我的金币</p>
+                </li>
+                <li>
+                  <i class="iconfont icon-shuji"></i>
+                  <p>我的书架</p>
+                </li>
+                <li>
+                  <i class="iconfont icon-lishi"></i>
+                  <p>浏览记录</p>
+                </li>
+              </ul>
+          </div>
+          <div class="my-body-item2">2</div>
+          <div class="my-body-item3">3 </div>
+          <div class="my-body-item4">4 </div>
 
-         </div>
-        </div>
+      </div>
+  </div>
 </template>
 
 <!-- scoped表示该样式是局域样式,不会影响其他组件 -->
