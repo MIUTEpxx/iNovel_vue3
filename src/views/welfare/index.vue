58773fa34c635de8aa79d4e2971d38305b493823
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-10-02 21:43:46 +0800

    福利页面

--- src/views/welfare/index.vue
+++ src/views/welfare/index.vue
@@ -4,8 +4,17 @@ import { ref } from 'vue';
 
 <template>
 
-        <div>
-                <p class="TempTest">福利</p>
+        <div class="welfare">
+                <div class="welfare-hd">
+                        <h3>登录账号, 天天领福利</h3>
+                        <button>登录</button>
+                </div>
+                
+                <div class="welfare-bod">
+                        <div class="welfare-body-item1">1</div>
+                        <div class="welfare-body-item2">2</div>
+                        <div class="welfare-body-item3">3</div>
+                </div>
 
         </div>
 </template>
