77333f926b78e1be0b8cc6b4f863eedbfed58b0a
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-10-09 11:15:58 +0800

    实现搜索按关键词,根据小说标题,作者名,简介信息来筛选小说

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -2,23 +2,45 @@
 <script setup>
 import HeaderHd from '@/components/header/HeaderHd.vue';
 import NovelItem from '@/components/novelInfo/NovelItem.vue';
-import { ref,onMounted } from 'vue';
-import { getNovelsInfo} from '@/api/novel'; // 获取标签和小说信息的API
+import { ref,onMounted ,watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { getNovelsInfo} from '@/api/novel'; // 获取小说信息的API
 
-let searchNovels=ref([])//筛选后的小说信息                                                            
+
+const route = useRoute();
+const keyword = ref(route.params.keyword || '');//获取搜索关键词
+const filteredNovels = ref([]);//筛选后的小说信息                                                            
 let novels=ref([]);//所有小说信息
 onMounted(async () => {
     try {
         const response=await getNovelsInfo(); // 获取全部小说的信息
         novels.value=response.data.data;
+        filterNovels();
     } catch (error) {
         console.error('Failed to fetch novel tags:', error);
     }
 
 });
+//监听搜索关键词
+watch(() => route.params.keyword, (newKeyword) => {
+  keyword.value = newKeyword;
+  filterNovels();
+});
+
+const filterNovels = () => {
+  if(keyword.value=='ALL'||keyword.value=='all'){
+    filteredNovels.value = novels.value;
+  }
+  else if (keyword.value) {
+    filteredNovels.value = novels.value.filter(novel => 
+      novel.title.includes(keyword.value) || novel.author.includes(keyword.value) || novel.summary.includes(keyword.value)
+    );
+  } else {
+    filteredNovels.value = []; // 如果没有关键字，则不显示任何结果
+  }
+};
 
 const activeSort = ref('default'); // 默认排序方式
-
 const handleSortChange = (name) => {
   // 根据选中的排序方式，重新获取或排序搜索结果
   // 例如，你可以根据 name 的值来调用不同的 API 或对本地数据进行排序
@@ -41,7 +63,7 @@ const handleSortChange = (name) => {
         </div>
         <div class="search-list">
                 <ul>
-                    <li v-for="(novel, index) in novels" :key="index">
+                    <li v-for="(novel, index) in filteredNovels" :key="index">
                         <NovelItem
                             :novelId="novel.id"
                             :novelImgUrl="novel.img_url"
